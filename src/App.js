de0af56210f57c84a61d12f9f338c00d7afe490d
YunusEmrePak <yunusemrepak@windowslive.com>
2023-09-19 22:01:36 +0300

    Find user by id

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const [number, setNumber] = useState(0);
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const idRef = useRef();
 
-  const increaseNumber = () => {
-    setNumber((prev) => prev + 1);
-  };
-
-  const decreaseNumber = () => {
-    setNumber((prev) => prev - 1);
+  const showId = () => {
+    fetch(`http://localhost:8080/user/get?id=${idRef.current.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.email === null) {
+          setError("User is not found!");
+          setData([]);
+          return;
+        } else {
+          console.log(data);
+          setData(data);
+        }
+      });
   };
 
   return (
     <div>
-      <div className="App">Hello Mehmet!</div>
-      <button onClick={increaseNumber}>Increase Number</button>
-      <button onClick={decreaseNumber}>Decrease Number</button>
-      <div>{number}</div>
+      <input ref={idRef} />
+      <button onClick={showId}>Show</button>
+      <div>Name: {data.name}</div>
+      <div>Surname: {data.surname}</div>
+      <div>Email: {data.email}</div>
+      <div>Length: {data.length}</div>
+      <div>Weight: {data.weight}</div>
+      <div>{error}</div>
     </div>
   );
 }
 
 export default App;
+
+// https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}
+
+// http://localhost:8080/user/get?id=50
