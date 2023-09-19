fe54895f96df8f9dd74aeb1a283ad500195af161
Yunus Emre Pak <yunusemrepak@windowslive.com>
2023-09-19 16:08:37 +0300

    Some functions are added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const increaseNumber = () => {
+    setNumber((prev) => prev + 1);
+  };
+
+  const decreaseNumber = () => {
+    setNumber((prev) => prev - 1);
+  };
+
   return (
-    <div className="App">
-      Hello Mehmet!
+    <div>
+      <div className="App">Hello Mehmet!</div>
+      <button onClick={increaseNumber}>Increase Number</button>
+      <button onClick={decreaseNumber}>Decrease Number</button>
+      <div>{number}</div>
     </div>
   );
 }
