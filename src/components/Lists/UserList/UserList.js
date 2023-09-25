d6d3a23c321d5f09071a3e6e91d1ff75fb6a57fe
YunusEmrePak <yunusemrepak@windowslive.com>
2023-09-25 21:48:48 +0300

    Font Size of Table changed

--- src/components/Lists/UserList/UserList.js
+++ src/components/Lists/UserList/UserList.js
@@ -16,12 +16,15 @@ import EatFitProContext from "../../store/context";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 
+const fontSize = 18;
+
 const UserList = () => {
   const context = useContext(EatFitProContext);
 
   const [userList, setUserList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [pageNumber, setPageNumber] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
 
   let userListSize = context.userListSize;
 
@@ -48,6 +51,7 @@ const UserList = () => {
       .then((data) => {
         setIsLoading(false);
         setUserList(data.content);
+        setTotalPage(data.totalPages);
       });
   }, [context.filterUserData, pageNumber, userListSize]);
 
@@ -68,21 +72,36 @@ const UserList = () => {
         {isLoading ? (
           <CircularProgress />
         ) : (
-          <TableContainer component={Paper} style={{ maxHeight: 350, marginTop: 20 }}>
+          <TableContainer
+            component={Paper}
+            style={{ maxHeight: 400, marginTop: 20 }}
+          >
             <Table
-              sx={{ minWidth: 650, maxHeight: 400 }}
+              sx={{ minWidth: 650 }}
               size="small"
               aria-label="a dense table"
-              style={{position: "relative"}}
             >
-              <TableHead
-              >
+              <TableHead>
                 <TableRow>
-                  <TableCell align="left">Name</TableCell>
-                  <TableCell align="left">Surname</TableCell>
-                  <TableCell align="left">Email</TableCell>
-                  <TableCell align="left">Length</TableCell>
-                  <TableCell align="left">Weight</TableCell>
+                  <TableCell
+                    align="left"
+                    size="small"
+                    style={{ fontSize: fontSize }}
+                  >
+                    Name
+                  </TableCell>
+                  <TableCell align="left" style={{ fontSize: fontSize }}>
+                    Surname
+                  </TableCell>
+                  <TableCell align="left" style={{ fontSize: fontSize }}>
+                    Email
+                  </TableCell>
+                  <TableCell align="left" style={{ fontSize: fontSize }}>
+                    Length
+                  </TableCell>
+                  <TableCell align="left" style={{ fontSize: fontSize }}>
+                    Weight
+                  </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -91,13 +110,26 @@ const UserList = () => {
                     key={row.email}
                     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
-                    <TableCell component="th" scope="row" align="left">
+                    <TableCell
+                      component="th"
+                      scope="row"
+                      align="left"
+                      style={{ height: 70, fontSize: fontSize }}
+                    >
                       {row.name}
                     </TableCell>
-                    <TableCell align="left">{row.surname}</TableCell>
-                    <TableCell align="left">{row.email}</TableCell>
-                    <TableCell align="left">{row.length}</TableCell>
-                    <TableCell align="left">{row.weight}</TableCell>
+                    <TableCell align="left" style={{ fontSize: fontSize }}>
+                      {row.surname}
+                    </TableCell>
+                    <TableCell align="left" style={{ fontSize: fontSize }}>
+                      {row.email}
+                    </TableCell>
+                    <TableCell align="left" style={{ fontSize: fontSize }}>
+                      {row.length}
+                    </TableCell>
+                    <TableCell align="left" style={{ fontSize: fontSize }}>
+                      {row.weight}
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
@@ -108,7 +140,7 @@ const UserList = () => {
 
       <Stack spacing={2} style={{ marginTop: 20 }}>
         <Pagination
-          count={10}
+          count={totalPage}
           color="primary"
           onChange={(event, page) => {
             if (page !== pageNumber) {
